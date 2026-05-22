06127909160f656d75491b9c7de7666b7e1bc1fa
rajd1005 <rajdey.dey1@gmail.com>
2026-05-22 18:43:59 +0530

    Update admin.routes.js

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -29,7 +29,8 @@ router.put('/settings', authenticateToken, isAdmin, async (req, res) => {
         show_disclaimer, register_link, push_trade_alerts, manager_emails,
         show_channel_tab,
         level_marketing_config,
-        login_popup_show, login_popup_title, login_popup_desc, login_popup_btn_text, login_popup_btn_link
+        login_popup_show, login_popup_title, login_popup_desc, login_popup_btn_text, login_popup_btn_link,
+        daily_sales_labels
     } = req.body;
     
     try {
@@ -78,6 +79,8 @@ router.put('/settings', authenticateToken, isAdmin, async (req, res) => {
         await pool.query("INSERT INTO system_settings (setting_key, setting_value) VALUES ('login_popup_desc', $1) ON CONFLICT (setting_key) DO UPDATE SET setting_value = EXCLUDED.setting_value", [login_popup_desc || '']);
         await pool.query("INSERT INTO system_settings (setting_key, setting_value) VALUES ('login_popup_btn_text', $1) ON CONFLICT (setting_key) DO UPDATE SET setting_value = EXCLUDED.setting_value", [login_popup_btn_text || '']);
         await pool.query("INSERT INTO system_settings (setting_key, setting_value) VALUES ('login_popup_btn_link', $1) ON CONFLICT (setting_key) DO UPDATE SET setting_value = EXCLUDED.setting_value", [login_popup_btn_link || '']);
+        
+        await pool.query("INSERT INTO system_settings (setting_key, setting_value) VALUES ('daily_sales_labels', $1) ON CONFLICT (setting_key) DO UPDATE SET setting_value = EXCLUDED.setting_value", [daily_sales_labels || '{}']);
 
         await redisClient.del('system_settings').catch(()=>{});
         req.app.get('io').emit('settings_updated');
